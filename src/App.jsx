753d9a1d29cc8ab1169e0d48a5026c10d3a73a5d
TabarakCodeCraft <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2025-12-20 21:36:58 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import ProductViewer from "./components/ProductViewer";
-import gsap from "gsap";
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <ProductViewer />
-    </main>
-  );
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
 }
 
-export default App;
+export default App
